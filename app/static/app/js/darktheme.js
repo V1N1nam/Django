245b2247e6523741b7a637162eb5e97ad3707444
Nearo007 <araujo8166@gmail.com>
2024-08-22 17:36:21 -0300

    darktheme imcompleto

--- app/static/app/js/darktheme.js
+++ app/static/app/js/darktheme.js
@@ -14,12 +14,32 @@ function changeTheme(switchToggle) {
     if (switchToggle === 1) {
         document.body.style.backgroundColor = 'black';
         document.body.style.color = 'white';
+        document.body.style.backgroundImage = 'linear-gradient(to left, rgba(49,0,8,1) 0%, rgba(14,14,14,1))';
+    
+        document.querySelectorAll('.titleRight').forEach(element => {
+            element.style.color = 'black';
+        });
+
+        document.querySelectorAll('.subIndexContainer').forEach(element => {
+            element.style.backgroundColor = 'black';
+        });
     }
+
     else {
         document.body.style.backgroundColor = 'white';
         document.body.style.color = 'black';
+        document.body.style.backgroundImage = 'linear-gradient(to right, rgb(235, 235, 235), rgb(225, 233, 241))';
+        
+        document.querySelectorAll('.titleRight').forEach(element => {
+            element.style.color = '';
+        });
+
+        document.querySelectorAll('.subIndexContainer').forEach(element => {
+            element.style.backgroundColor = 'white';
+        });
     }
 }
+
 
 const switchButton = document.querySelector('.darkthemeButton');
 switchButton.addEventListener('click', () => {
