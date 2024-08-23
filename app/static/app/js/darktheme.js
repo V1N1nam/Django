1db01e1b11106b542b2164fc637dabce3b576fba
Seu Nome <Seu Nome>
2024-08-23 16:11:19 -0300

    tema escuro quase finalizado

--- app/static/app/js/darktheme.js
+++ app/static/app/js/darktheme.js
@@ -12,43 +12,62 @@ function toggle() {
 
 function changeTheme(switchToggle) {
     if (switchToggle === 1) {
-        document.body.style.backgroundColor = 'black';
         document.body.style.color = 'white';
-        document.body.style.backgroundImage = 'linear-gradient(to left, rgba(49,0,8,1) 0%, rgba(14,14,14,1))';
+        document.body.style.backgroundImage = 'none';
+        document.body.style.backgroundColor = 'rgb(30,33,36)';
     
         document.querySelectorAll('.titleRight').forEach(element => {
-            element.style.color = 'black';
+            element.style.color = 'rgb(236, 43, 75)';
+        });
+
+        document.querySelectorAll('.indexContainer').forEach(element => {
+            element.style.border = '2px solid rgb(236, 43, 75)';
         });
 
         document.querySelectorAll('.subIndexContainer').forEach(element => {
-            element.style.backgroundColor = 'black';
+            element.style.backgroundColor = 'transparent';
+        });
+
+        document.querySelectorAll('.subIndexContainer2').forEach(element => {
+            element.style.backgroundImage = 'none';
+            element.style.borderLeft = '2px solid rgb(236, 43, 75)';
         });
 
         document.querySelectorAll('.subTitleRight').forEach(element => {
-            element.style.color = 'black';
+            element.style.color = 'rgb(236, 43, 75)';
         });
 
         document.querySelectorAll('.signinBtn').forEach(element => {
-            element.style.color = 'black';
+            element.style.color = 'white';
         });
 
         document.querySelectorAll('.signupBtn').forEach(element => {
-            element.style.color = 'black';
-            element.style.border = '2px solid black';
+            element.style.color = 'rgb(236, 43, 75)';
+            element.style.border = '2px solid rgb(236, 43, 75)';
         });
     }
 
     else {
         document.body.style.backgroundColor = 'white';
-        document.body.style.color = 'black';
+        document.body.style.color = 'rgb(236, 43, 75)';
         document.body.style.backgroundImage = 'linear-gradient(to right, rgb(235, 235, 235), rgb(225, 233, 241))';
         
         document.querySelectorAll('.titleRight').forEach(element => {
             element.style.color = '';
         });
 
+        document.querySelectorAll('.indexContainer').forEach(element => {
+            element.style.border = 'none';
+        });
+
         document.querySelectorAll('.subIndexContainer').forEach(element => {
             element.style.backgroundColor = 'white';
+        });
+
+        document.querySelectorAll('.subIndexContainer2').forEach(element => {
+            element.style.backgroundColor = 'transparent';
+            element.style.backgroundImage = 'linear-gradient(to top, rgb(182, 37, 37), rgb(236, 43, 75))';
+            element.style.border = 'none';
         });
 
         document.querySelectorAll('.subTitleRight').forEach(element => {
