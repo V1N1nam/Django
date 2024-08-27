1e7f490c921d6cd336f98d80f7d374b72df9bd56
Nearo007 <araujo8166@gmail.com>
2024-08-27 10:04:42 -0300

    add delay js

---
+++ app/static/app/js/redirectDelay.js
@@ -0,0 +1,19 @@
+function redirectWithDelay(url) {
+    // Pré-carrega a página para cachear alguns recursos
+    const preloader = document.createElement('link');
+    preloader.rel = 'prefetch';
+    preloader.href = url;
+    document.head.appendChild(preloader);
+    
+    // Aplica o delay antes de redirecionar visualmente
+    setTimeout(() => {
+        window.location.href = url;
+    }, 500);
+}
+
+
+/*function redirectWithDelay(url, delay) {
+    setTimeout(() => {
+        window.location.href = url;
+    }, delay);
+}*/
