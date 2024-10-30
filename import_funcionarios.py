c37d6d081172df472b24c68263e2e6018f979dd2
Nearo007 <araujo8166@gmail.com>
2024-10-30 13:49:06 -0300

    better skills database

--- import_funcionarios.py
+++ import_funcionarios.py
@@ -12,18 +12,26 @@ django.setup()  # Inicializa o Django
 
 def importar_funcionarios():
     # Importa o modelo após a configuração do Django
-    from app.models import Funcionario
+    from app.models import Funcionario, Skill
     
     try:
         with open('funcionarios.json', encoding='utf-8') as f:
             data = json.load(f)
             for entry in data:
                 fields = entry['fields']
-                Funcionario.objects.create(
+                funcionario = Funcionario.objects.create(
                     nome=fields['nome'],
                     cargo=fields['cargo'],
-                    data_nascimento=fields['data_nascimento']
+                    data_nascimento=fields['data_nascimento'],
                 )
+            
+                skills_names = fields.get('skills', [])
+                skills = []
+
+                for skill_name in skills_names:
+                    skill, created = Skill.objects.get_or_create(nome=skill_name)
+                    skills.append(skill)
+                    funcionario.skills.set(skills)
         print('Funcionários importados com sucesso!')
     
     except Exception as e:
