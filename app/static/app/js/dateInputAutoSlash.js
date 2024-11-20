045f769d1cf10c1440356efaa0cb410c670c9168
Nearo007 <araujo8166@gmail.com>
2024-11-20 08:18:57 -0300

    a

--- app/static/app/js/dateInputAutoSlash.js
+++ app/static/app/js/dateInputAutoSlash.js
@@ -1,9 +1,7 @@
 document.getElementById('id_data_nascimento').addEventListener('input', function (e) {
     let input = e.target.value;
-    // Remove qualquer caractere não numérico
     input = input.replace(/\D/g, '');
 
-    // Adiciona as barras automaticamente
     if (input.length > 2 && input.length <= 4) {
         input = input.slice(0, 2) + '/' + input.slice(2);
     } else if (input.length > 4) {
